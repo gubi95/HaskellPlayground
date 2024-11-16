beb965f5116feafc6d8bc356de143b413a7d2d0b
gubi95 <rafalgubala01@gmail.com>
2024-11-16 21:22:55 +0100

    Proper DB model for airports

--- SkyScannerSimulator/db/scripts/003_create_tables.sql
+++ SkyScannerSimulator/db/scripts/003_create_tables.sql
@@ -10,15 +10,28 @@ CREATE TABLE [SimulatorService].[Plane] (
 INSERT INTO [SimulatorService].[Plane] ([Model], [Airlines], [DistancePerTick]) VALUES 
 ('Boeing 777', 'WizzAir', 10), ('Airbus A320', 'WizzAir', 12), ('Airbus A380', 'Ryanair', 14)
 
+CREATE TABLE [SimulatorService].[Airport] (
+    [Id] INT NOT NULL IDENTITY PRIMARY KEY,
+    [Code] NVARCHAR(10) NOT NULL,
+    [Lat] NVARCHAR(30) NOT NULL,
+    [Lon] NVARCHAR(30) NOT NULL
+)
+
+INSERT INTO [SimulatorService].[Airport] ([Code], [Lat], [Lon])
+VALUES ('JFK', '40_38_23', '73_46_44'), ('LAX', '33_56_33', '118_24_29')
+
 CREATE TABLE [SimulatorService].[Flights] (
-    [From] NVARCHAR(100) NOT NULL,
-    [To] NVARCHAR(100) NOT NULL,
     [PlaneId] INT NOT NULL,
+    [DepartureAirportId] INT NOT NULL,
+    [ArrivalAirportId] INT NOT NULL,
 
-    CONSTRAINT FK_Plane_Flights FOREIGN KEY ([PlaneId]) REFERENCES SimulatorService.Plane([Id])
+    CONSTRAINT FK_Plane_Flights FOREIGN KEY ([PlaneId]) REFERENCES SimulatorService.Plane([Id]),
+    CONSTRAINT FK_AirportDeparture_Flights FOREIGN KEY ([DepartureAirportId]) REFERENCES SimulatorService.Airport([Id]),
+    CONSTRAINT FK_AirportArrival_Flights FOREIGN KEY ([ArrivalAirportId]) REFERENCES SimulatorService.Airport([Id])
 )
 
-INSERT INTO [SimulatorService].[Flights] (
-    [From], [To], [PlaneId]
-)
-VALUES ('New York', 'Los Angeles', (SELECT [Id] FROM [SimulatorService].[Plane] WHERE [Model] = 'Boeing 777'))
+INSERT INTO [SimulatorService].[Flights] ([PlaneId], [DepartureAirportId], [ArrivalAirportId])
+VALUES (
+    (SELECT [Id] FROM [SimulatorService].[Airport] WHERE [Code] = 'JFK'),
+    (SELECT [Id] FROM [SimulatorService].[Airport] WHERE [Code] = 'LAX'),
+    (SELECT [Id] FROM [SimulatorService].[Plane] WHERE [Model] = 'Boeing 777'))

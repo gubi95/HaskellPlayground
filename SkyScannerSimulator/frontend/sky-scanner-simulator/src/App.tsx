b1984d5f020b3cf2ea75453b5cc965dae1e8e2b3
gubi95 <rafalgubala01@gmail.com>
2024-12-11 21:16:31 +0100

    Draw simple map

--- SkyScannerSimulator/frontend/sky-scanner-simulator/src/App.tsx
+++ SkyScannerSimulator/frontend/sky-scanner-simulator/src/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import './App.css';
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="MapWrapper">
+        <ComposableMap >
+          <Geographies geography="/features.json">
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography key={geo.rsmKey} geography={geo} fill="#FF5533" stroke="#000000" />
+              ))
+            }
+          </Geographies>
+        </ComposableMap>
+      </div>
     </div>
   );
 }

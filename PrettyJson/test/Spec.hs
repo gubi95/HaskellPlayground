d81aba575eeec836700d93310ad07e1b35445b1d
gubi95 <rafalgubala01@gmail.com>
2026-07-07 13:57:03 +0200

    Tests

--- PrettyJson/test/Spec.hs
+++ PrettyJson/test/Spec.hs
@@ -1,2 +1,34 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import JsonParser (JsonToken (..), parse)
+import Test.HUnit
+
+main :: IO Counts
+main =
+  runTestTT $
+    TestList
+      [ TestLabel
+          "Should parse sinle property object"
+          ( TestCase
+              ( do
+                  let json =
+                        "{\
+                        \\"test\":\
+                        \\"value\"\
+                        \}"
+
+                  let expected =
+                        Just
+                          [ LeftCurlyBracket,
+                            DoubleQuote,
+                            Property "test",
+                            DoubleQuote,
+                            Colon,
+                            DoubleQuote,
+                            StringValue "value",
+                            DoubleQuote,
+                            RightCurlyBracket
+                          ]
+
+                  assertEqual "" expected (JsonParser.parse json)
+              )
+          )
+      ]
